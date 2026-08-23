cbe7c4a0055e72ca7e8724aed82ef3433f53b364
mixmasterkd <mixmasterkd@gmail.com>
2026-08-23 16:32:18 -0400

    Publier la connexion Supabase et l administration

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.APP_DASHBOARD_PATH = "./";
+window.supabaseClient = window.supabase.createClient(
+  "https://opxsaykcofzbufzzzqwz.supabase.co",
+  "sb_publishable_5iSjD5Zi9-eSGtX0f2oL4w_N-NdZKcZ"
+);
